e26bfaf76bbe1f77996c5f66e7460cf9146f0374
GilLog <swgil007@naver.com>
2022-02-09 06:00:52 +0900

    Update DBHelp.java

--- app/src/main/java/com/gil/gyrotouch/DBHelp.java
+++ app/src/main/java/com/gil/gyrotouch/DBHelp.java
@@ -7,35 +7,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelp extends SQLiteOpenHelper {
 
-    // DBHelper 생성자로 관리할 DB 이름과 버전 정보를 받음
     public DBHelp(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
-    // DB를 새로 생성할 때 호출되는 함수
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // 새로운 테이블 생성
-        /* 이름은 MONEYBOOK이고, 자동으로 값이 증가하는 _id 정수형 기본키 컬럼과
-        item 문자열 컬럼, price 정수형 컬럼, create_at 문자열 컬럼으로 구성된 테이블을 생성. */
-        //db.execSQL("CREATE TABLE MEASUREBOX (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price INTEGER);");
         db.execSQL("CREATE TABLE MEASUREBOX (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, num INTEGER, mnum INTERGER, mtx REAL, mty REAL, mgx REAL, mgy REAL, mgz REAL, dt REAL);");
         db.execSQL("CREATE TABLE BALANCEBOX (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, mnum INTERGER, btx REAL, bty REAL, bgx REAL, bgy REAL, bgz REAL, bdt REAL);");
         db.execSQL("CREATE TABLE COSINEBOX (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, num INTEGER, mnum INTERGER, ctx REAL, cty REAL, cgx REAL, cgy REAL, cgz REAL, cdt REAL);");
         db.execSQL("CREATE TABLE VALIDBOX (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, valid1min REAL, valid1max REAL, valid2min REAL, valid2max REAL, valid3min REAL, valid3max REAL, valid4min REAL, valid4max REAL);");
     }
 
-    // DB 업그레이드를 위해 버전이 변경될 때 호출되는 함수
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
 
     public String findResult(String id, int num) {
-        // 읽기가 가능하게 DB 열기
         SQLiteDatabase db = getReadableDatabase();
         String findresult = "";
 
-        // DB에 있는 데이터를 쉽게 처리하기 위해 Cursor를 사용하여 테이블에 있는 모든 데이터 출력
         Cursor cursor = db.rawQuery("SELECT * FROM MEASUREBOX WHERE name = '" + id + "' AND num = '" + num + "';", null);
         while (cursor.moveToNext()) {
             findresult += cursor.getString(0)
