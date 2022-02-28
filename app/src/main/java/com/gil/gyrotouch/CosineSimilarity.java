c3957abc43b48ed13581ad7a53fa06f81814b14f
GilLog <swgil007@naver.com>
2022-03-01 01:09:40 +0900

    Update CosineSimilarity.java

--- app/src/main/java/com/gil/gyrotouch/CosineSimilarity.java
+++ app/src/main/java/com/gil/gyrotouch/CosineSimilarity.java
@@ -2,17 +2,14 @@ package com.gil.gyrotouch;
 
 public class CosineSimilarity {
     public static double cosinsimility(float[] baltest1, float[] mea1){
-
         float head = 0;
         float mnohat = 0;
         float bnohat = 0;
         double body = 0;
         double cosmlity = 0;
-
         for(int i = 0; i<6; i++){
             head += baltest1[i] * mea1[i];
         }
-
         for(int i = 0; i<6; i++){
             mnohat +=  mea1[i] * mea1[i];
             bnohat += baltest1[i] * baltest1[i];
